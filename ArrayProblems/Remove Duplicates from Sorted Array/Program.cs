394ffc77daf6b519006cf893e3194948d806dc9c
m-anass <manas8269@gmail.com>
2024-08-18 08:23:40 +0300

    Remove Duplicates from Sorted Array

---
+++ ArrayProblems/Remove Duplicates from Sorted Array/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Remove_Duplicates_from_Sorted_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 1, 1, 2 };
+            Console.WriteLine(RemoveDuplicates(nums));
+        }
+        public static int RemoveDuplicates(int[] nums)
+        {
+            int size = 0;
+            int current = int.MinValue;
+            foreach (var item in nums)
+            {
+                if(current != item)
+                {
+                    size++;
+                    nums[size - 1] = item;
+                    current = item;
+                }
+            }
+            return size;
+        }
+    }
+}
